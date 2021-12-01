65b4a802a751485cc064c2be71b6020908c1144d
tongoonamujera <tongoonamujera@gmail.com>
2021-12-01 12:42:57 +0200

    Fix linter errors in webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   entry: {
@@ -24,7 +24,7 @@ module.exports = {
     open: true,
     port: 9002,
     proxy: {
-      '/api': 'http://localhost:9000'
+      '/api': 'http://localhost:9000',
     },
   },
   module: {
@@ -35,14 +35,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
-      { 
-        test: /\.css$/, 
-        use: ["style-loader", "css-loader"] 
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
-    ]
+    ],
   },
 };
