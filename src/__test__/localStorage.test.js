f913f6e7e73bd733366de83bcce7aed1e890175b
tongoonamujera <tongoonamujera@gmail.com>
2021-12-09 11:35:37 +0200

    Add some tests

--- src/__test__/localStorage.test.js
+++ src/__test__/localStorage.test.js
@@ -3,15 +3,20 @@
  */
 
 import TaskStorage from '../localStorage';
+import MockStorage from '../__mocks__/StorageMock';
 
 describe('clases should return objects', () =>{
+  window.localStorage = MockStorage;
+  window.sessionStorage = MockStorage;
   const tasks = {
     description: 'tongoona',
     completed: false,
     index: 0,
-    }
+  };
+
   const func = new TaskStorage;
-  test('should be a object', () =>{
+  const Mockfn = jest.fn(func.addTask(tasks));
+  test('should be a object', () => {
     expect(typeof func.getTask()).toBe('object');
   });
 
@@ -19,7 +24,11 @@ describe('clases should return objects', () =>{
     expect(typeof func.addTask(tasks)).toBe('undefined');
   });
 
-  test('should not return anyting other than undefined if no localStorage defined', () => {
+  test('should not be called', () => {
+    expect(Mockfn).not.toHaveBeenCalled();
+  });
+
+  test('should not return anything other than undefined if no localStorage defined', () => {
     expect(typeof func.addTask(tasks)).not.toBe('object');
   });
 
