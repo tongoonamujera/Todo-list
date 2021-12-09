456a324cb26bf90c66a3a32e15d53be75a05a328
cwaku <rickymormor1@gmail.com>
2021-12-09 13:32:47 +0000

    Fix Linter errors

--- src/__mocks__/StorageMock.js
+++ src/__mocks__/StorageMock.js
@@ -1,13 +1,13 @@
 const MockStorage = () => {
   const storage = {};
   return {
-    setItem (key, value) {
+    setItem(key, value) {
       storage[key] = value || '';
     },
-    getItem (key, value) {
+    getItem(key, value) {
       return key in storage ? storage[key] : null;
     },
-    key (i) {
+    key(i) {
       const keys = Object.keys(storage);
       return keys[i] || null;
     },
