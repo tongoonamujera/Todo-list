88b2fbf8f3b27661faea21ca4a74efd7bb9d4e4e
cwaku <rickymormor1@gmail.com>
2021-12-09 13:29:45 +0000

    Fix Linter Issues

--- src/__test__/localStorage.test.js
+++ src/__test__/localStorage.test.js
@@ -36,7 +36,7 @@ describe('LocalStorage', () => {
   });
 
   test('should return length of localStorage', () => {
-    expect(Object.keys(localStorage.STORE).length).toBe(localStorage.length);
+    expect(Object.keys(localStorage.__STORE__).length).toBe(localStorage.length);
   });
 
   test('should be querry for items in localStorage', () => {
